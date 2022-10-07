2a4481e04c43aa84c007269dabe89a82a526e8c4
AmuthaSethuramaraj <114208943+AmuthaSethuramaraj@users.noreply.github.com>
2022-10-07 10:41:18 +0530

    Add files via upload

---
+++ AppTest.java
@@ -0,0 +1,13 @@
+package TC_amazon.TC_Amazon;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppTest {
+    
+  @Test
+  public void shouldAnswerWithTrue() {
+    assertTrue(true);
+  }
+}
